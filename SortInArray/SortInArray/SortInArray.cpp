47508c32b4461443cf424805ed25f7c080916b59
James Kim <yt4921@gmail.com>
2025-03-19 13:47:23 -0700

    create the insertion sort function and put it in the Sort class

--- SortInArray/SortInArray/SortInArray.cpp
+++ SortInArray/SortInArray/SortInArray.cpp
@@ -1,10 +1,42 @@
 #include <iostream>
 using namespace std;
 
+class Sort {
+private:
+	int capacity;
+	int size;
+	int A[10];
+public:
+	Sort() {
+		this->capacity = 10;
+		this->size = 0;
+	}
+
+
+
+	void sortInInsert(int* A) {
+		for (int i = 1; i < this->capacity; i++) {
+			int pivot = A[i];
+			int j = i - 1;
+			while (j >= 0 && A[j] > pivot) {
+				A[j + 1] = A[j];
+				j = j - 1;
+			}
+			A[j + 1] = pivot;
+		}
+		return;
+	}
+
+};
+
+
+
+
 
 int main() {
-
-	cout << "Hello world";
+	int A[10];
+	sortInInsert(A);
+	
 
 	return 0;
 }
